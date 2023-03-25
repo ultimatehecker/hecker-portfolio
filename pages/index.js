6e346aa103da85d08585369a287db8b05209d339
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-25 14:04:03 -0400

    intergrate with chakra-ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
     return (
-        <div>Hello World</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Namaste, I&apos;m a versatile developer based in India!
+            </Box>
+            
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant={'page-title'}>
+                        Hecker&apos;s Portfolio
+                    </Heading>
+
+                    <p>Digital Enthusiast ( Professional Gamer / Devloper )</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
